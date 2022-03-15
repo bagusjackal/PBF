faea33d1cd868dd17414e8882dc6bf6cc744bc3d
bagusjackal <faridbagus14@gmail.com>
2022-03-15 10:16:13 +0700

    modul 4

---
+++ modul 4/web-reactku/src/App.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import Footer from './Footer';
+import List from './List';
+
+
+class App extends Component {
+ render() {
+    return (
+      <div>
+      <h1>Component dari Class App</h1>
+      <List/>
+      <Footer judul='Halaman Footer' nama='Farid Bagus Setiawan' />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ modul 4/web-reactku/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BlogPost from './container/BlogPost/BlogPost';
+
+
+
+
+
+ReactDOM.render(<BlogPost/>, document.getElementById('content'));
+
+
+
+
+
+reportWebVitals();
