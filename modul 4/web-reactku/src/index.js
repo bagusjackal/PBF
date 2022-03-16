873eef493a5fd3634ad6b335f71e7edd6c6a8a87
bagusjackal <faridbagus14@gmail.com>
2022-03-16 13:21:26 +0700

    modul 4

--- modul 4/web-reactku/src/index.js
+++ modul 4/web-reactku/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BlogPost from './container/BlogPost/BlogPost';
+// import BlogPost from './container/BlogPost/BlogPost';
+import MahasiswaBlogPost from './container/BlogPost/MahasiswaBlogPost';
 
 
 
 
-
-ReactDOM.render(<BlogPost/>, document.getElementById('content'));
-
-
+// ReactDOM.render(<BlogPost/>, document.getElementById('content'));
+ReactDOM.render(<MahasiswaBlogPost/>, document.getElementById('content'));
 
 
 
-reportWebVitals();
+
+// reportWebVitals();
